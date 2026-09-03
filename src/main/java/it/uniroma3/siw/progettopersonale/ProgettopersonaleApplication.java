bdbf768626f5f7f08a5af00763d9daeece62c7c5
alegrasso3 <ale.grasso3@stud.uniroma3.it>
2026-09-03 18:13:39 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/progettopersonale/ProgettopersonaleApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progettopersonale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettopersonaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettopersonaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/progettopersonale/ProgettopersonaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progettopersonale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettopersonaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
